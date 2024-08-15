83e8158ef06dc76ba6da69a4f31fc44fee3d5d40
Haris Ayyaz <harisayyaz@gmail.com>
2024-08-15 21:32:04 +0500

    Print function

---
+++ Print-Function.py
@@ -0,0 +1,33 @@
+#1 Basic Printing
+print("Hello, World!")
+print("Haris")
+
+#2 Printing Multiple Items
+print("Haris", "Ayyaz")
+print(1, 2, 3)
+
+#3 Printing Special Characters
+print("Hello\nWorld")
+print("Hello\tWorld")
+
+#4 Using the sep Parameter
+print("apple", "banana", "cherry", sep=", ")
+print(1, 2, 3, sep="-")
+
+#5 Using the end Parameter
+print("Hello", end=" ")
+print("World")
+print(1, 2, end="")
+
+#6 Escape Characters
+print("He said, \"Hello!\"")
+print("This is a backslash: \\")
+
+#7 Combining Text and Numbers
+age = 20
+print("I am", age, "years old")
+print("The number is", 5)
+
+#8 Basic Loop for Printing
+for number in range(1, 6):
+    print(number)
